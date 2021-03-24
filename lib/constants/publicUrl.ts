7cb74c192df6977c61e97103c5f6034b1aa8d033
Luke Murray <lukepigeonmail@gmail.com>
2021-03-23 21:23:46 -0400

    make url have https

--- lib/constants/publicUrl.ts
+++ lib/constants/publicUrl.ts
@@ -1,2 +1,4 @@
 export const publicUrl =
-  process.env.NODE_ENV === "development" ? "localhost:3000" : "lsmurray.com";
+  process.env.NODE_ENV === "development"
+    ? "localhost:3000"
+    : "https://lsmurray.com";
