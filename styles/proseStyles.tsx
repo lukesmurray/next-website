fd02e6faa2a82cde61394a2cf3dce986c493f8b7
Luke Murray <lukepigeonmail@gmail.com>
2021-03-26 18:06:24 -0400

    remove small text

--- styles/proseStyles.tsx
+++ styles/proseStyles.tsx
@@ -4,7 +4,7 @@ import tw, { css } from "twin.macro";
  * styling for prose so that all prose elements use the same breakpoints
  */
 export const proseStyles = css`
-  ${tw`prose prose-sm sm:prose lg:prose-lg`}
+  ${tw`prose sm:prose lg:prose-lg`}
 
   /* remove backticks */
   code {
@@ -21,7 +21,7 @@ export const proseStyles = css`
 /**
  * styling for text so all text widths are the same as the prose widths
  */
-export const textSizes = tw`text-sm sm:text-base lg:text-lg`;
+export const textSizes = tw`text-base sm:text-base lg:text-lg`;
 
 export const h1Styles = tw`text-4xl sm:text-5xl lg:text-6xl font-extrabold text-gray-900`;
 export const h2Styles = tw`text-2xl sm:text-3xl lg:text-4xl font-bold text-gray-900`;
