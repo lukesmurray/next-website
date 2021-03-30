eb931fdb445a31c16764d5248f05b6ae48f1dd31
Luke Murray <lukepigeonmail@gmail.com>
2021-03-29 20:59:15 -0400

    flashless darkmode

--- components/theme/ThemeContext.tsx
+++ components/theme/ThemeContext.tsx
@@ -1,57 +1,41 @@
 import React, { useContext } from "react";
 
-type Theme = "light" | "dark";
-const isTheme = (val: any): val is Theme => {
-  return typeof val === "string" && (val === "light" || val === "dark");
-};
-
-const getInitialTheme = (): Theme => {
-  if (typeof window !== "undefined" && window.localStorage) {
-    const storedPrefs = window.localStorage.getItem("color-theme");
-    if (isTheme(storedPrefs)) {
-      return storedPrefs;
-    }
-
-    const userMedia = window.matchMedia("(prefers-color-scheme: dark)");
-    if (userMedia.matches) {
-      return "dark";
-    }
-  }
-
-  return "dark";
-};
-
 type ThemeContext = {
   theme: Theme;
   setTheme: (theme: Theme) => void;
 };
 const ThemeContext = React.createContext<ThemeContext | undefined>(undefined);
 
-interface ThemeProviderProps {
-  initialTheme?: Theme;
-}
+const ThemeProvider: React.FC = (props) => {
+  const { children } = props;
 
-const ThemeProvider: React.FC<ThemeProviderProps> = (props) => {
-  const { initialTheme, children } = props;
-
-  const [theme, setTheme] = React.useState<Theme>(() => getInitialTheme());
-
-  const rawSetTheme = (theme: Theme) => {
-    const root = window.document.documentElement;
-    const isDark = theme === "dark";
-
-    root.classList.remove(isDark ? "light" : "dark");
-    root.classList.add(theme);
-
-    localStorage.setItem("color-theme", theme);
-  };
-
-  if (initialTheme) {
-    rawSetTheme(initialTheme);
-  }
+  const [theme, setTheme] = React.useState<Theme>(() => {
+    if (typeof window !== "undefined") {
+      return window.__theme;
+    }
+    return "light";
+  });
 
   React.useEffect(() => {
-    rawSetTheme(theme);
+    let __onThemeChange: typeof window.__onThemeChange | undefined = undefined;
+    if (typeof window !== "undefined") {
+      __onThemeChange = window.__onThemeChange;
+      window.__onThemeChange = (newTheme) => setTheme(newTheme);
+    }
+
+    return () => {
+      if (typeof window !== "undefined" && __onThemeChange !== undefined) {
+        window.__onThemeChange = __onThemeChange;
+      }
+    };
+  }, []);
+
+  React.useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (theme !== window.__theme) {
+        window.__setPreferredTheme(theme);
+      }
+    }
   }, [theme]);
 
   return (
--- global.d.ts
+++ global.d.ts
@@ -5,3 +5,11 @@ declare module "*.jpeg";
 declare module "*.jpg";
 declare module "*.gif";
 declare module "*.mp4";
+
+type Theme = "dark" | "light";
+
+declare interface Window {
+  __theme: Theme;
+  __onThemeChange: (newTheme: Theme) => void;
+  __setPreferredTheme: (newTheme: Theme) => void;
+}
